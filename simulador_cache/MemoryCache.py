85c884f81d17f81aa27b8109d624f69a5101e0fe
fersasil <guilhermefersasil@gmail.com>
2019-06-29 16:45:06 -0300

    ok

--- simulador_cache/MemoryCache.py
+++ simulador_cache/MemoryCache.py
@@ -10,16 +10,23 @@ Cada linha será um objeto do tipo Line que possui um rótulo e quatro células
 class MemoryCache:
     def __init__(self):
         self.line = []
-        #cada linha tem o tamanho de um bloco
+        # cada linha tem o tamanho de um bloco
         # logo cada linha sera composta de 4 celulas
         for i in range(8):                
             self.line.append(Line(i))
     
     def printAllCache(self):
-        for l in self.line:
-            for cell in l:
+        for i, l in enumerate(self.line):
+            quadro = None
+            
+            if i < 4:
+                quadro = "A"
+            else:
+                quadro = "B"
+            
+            print("Quadro {} - Label: {}".format(quadro, l.label))
+            
+            for cell in l.line:
                 cell.printCell()
                 
-            print() 
-            
-    pass
+            print()
