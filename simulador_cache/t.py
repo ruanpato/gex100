eef4ba52338f6f6724e68b0de197616be9f1da52
fersasil <guilhermefersasil@gmail.com>
2019-06-29 19:26:59 -0300

    oi

--- simulador_cache/t.py
+++ simulador_cache/t.py
@@ -23,4 +23,35 @@ for i in range(32):
     
     res = zeros + bits + deslocamento
     
-    print("{} = {}".format(i, res))
+    #print("{} = {}".format(i, res))
+    #print(int(res[: 6], 2))
+
+    
+
+# 5 digitos mais significativos indica o rotulo
+# 01111 111 11 
+
+
+
+for i in range(128):
+    bits = bin(i)[2:]
+
+    numBits = len(bits)
+    
+    zeros = ""
+    
+    if(numBits < 8):
+        dif = 7 - numBits
+        
+        for j in range(dif):
+            zeros += "0"
+    
+    res = zeros + bits
+
+    #print(int(res[:5], 2))
+    #print(res[:5])
+    # BBB BB PP DD
+    print(int(res[-4:-2], 2))
+    
+    
+    #print(ultimos)
