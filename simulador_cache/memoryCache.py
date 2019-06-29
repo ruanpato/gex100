8afe80b87e291a510ea0e0ddcd54b41c69b0882e
ruanpato <ruanpato@outlook.com>
2019-06-29 15:38:14 -0300

    divide class

---
+++ simulador_cache/memoryCache.py
@@ -0,0 +1,30 @@
+class MemoryCache:
+    def __init__(self):
+        self.line = []
+        #cada linha tem o tamanho de um bloco
+        # logo cada linha sera composta de 4 celulas
+
+        for i in range(8):                
+            l = [Cell(), Cell(), Cell(), Cell()]
+            self.line.append(l)
+    
+    def printAllCache(self):
+        for l in self.line:
+            for cell in l:
+                cell.printCell()
+                
+            print()
+            
+    pass
+
+def readContentFromMemory():
+    
+    pass
+
+def writeContentInMemory():
+    
+    pass
+
+def statistics():
+    
+    pass
