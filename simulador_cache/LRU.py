5466de84b4fa8ed78ba8275ae25a220fb1e467f9
ruanpato <ruanpato@outlook.com>
2019-07-03 19:57:32 -0300

    Just little changes in LRU class

--- simulador_cache/LRU.py
+++ simulador_cache/LRU.py
@@ -1,4 +1,20 @@
 import MemoryCache
 import bitarray
 
+'''
+O mais recentemente usado fica com 1
+'''
+
 class LRU(object):
+    def __init__(self, cache, block, label):
+        self.cache = cache  # Recebe um objeto do tipo cache
+        self.block = block
+        self.label = label
+
+    def verificacoes(self, quadroA, label):
+        # Verifica se está na cache
+        if self.label == self.cache.lines[quadroA].label:
+            aux = self.cache.lines[]
+
+        if self.label == self.cache.lines[quadroB].label:
+            aux = self.cache.lines[]
