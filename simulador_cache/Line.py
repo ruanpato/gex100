39090fa46affe86519edffd49abe7ad2613805d8
ruanpato <ruanpato@outlook.com>
2019-07-03 19:56:22 -0300

    just a class change

--- simulador_cache/Line.py
+++ simulador_cache/Line.py
@@ -4,3 +4,10 @@ class Line:
     def __init__(self, label):
         self.line = [Cell(), Cell(), Cell(), Cell()]
         self.label = label
+        self.recentlyUsed = 0
+
+    def setLabel(self, label):
+        self.label = label
+    
+    def setRecentlyUsed(self, recentlyUsed):
+        self.recentlyUsed = recentlyUsed
